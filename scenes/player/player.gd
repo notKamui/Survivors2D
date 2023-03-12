76120e27d56caee143cd8e84b61bd29302101fd4
notKamui <teillard.jimmy@outlook.fr>
2023-03-12 22:25:08 +0100

    Player movement

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,14 +1,20 @@
 extends CharacterBody2D
 
+
 @export var max_speed := 200
 
-func _process(delta):
-	pass
+
+func _physics_process(delta):
+	var movement_vector = _get_movement_vector()
+	var direction = movement_vector.normalized()
+	velocity = direction * max_speed * (delta * 50)
+	move_and_slide()
+
 
 func _get_movement_vector() -> Vector2:
 	return Input.get_vector(
 		"move_left", 
 		"move_right",
-		"move_down",
-		"move_up"
+		"move_up",
+		"move_down"
 	)
